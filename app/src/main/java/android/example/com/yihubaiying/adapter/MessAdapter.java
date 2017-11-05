db879ba0fb7f781c0b6712bfc4e3de5aed40b7d7
Thingvellir <315338868@qq.com>
2017-11-06 00:03:29 +0800

    合并工作

--- app/src/main/java/android/example/com/yihubaiying/adapter/MessAdapter.java
+++ app/src/main/java/android/example/com/yihubaiying/adapter/MessAdapter.java
@@ -11,13 +11,12 @@ import java.util.List;
  * Created by Thingvellir on 2017/11/2.
  */
 
-public class MessAdapter extends BaseQuickAdapter<MessItem,BaseViewHolder> {
+public class MessAdapter extends BaseQuickAdapter<Message,BaseViewHolder> {
 
     public MessAdapter(int layoutResId,List data){
         super(layoutResId,data);
     }
-    @Override
-    protected void convert(BaseViewHolder helper, MessItem item) {
+    protected void convert(BaseViewHolder helper, Message item) {
         helper.setImageResource(R.id.mess_user,item.getUserImageId()).setText(R.id.mess_title,item.getTitle());
         helper.setText(R.id.mess_content,item.getContent()).setText(R.id.mess_time,item.getTime());
         helper.setImageResource(R.id.mess_state,item.getStateImageId());
