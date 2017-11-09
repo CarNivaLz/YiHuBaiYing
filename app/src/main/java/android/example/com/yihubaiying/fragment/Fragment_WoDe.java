8abfcee335ac510189143d1c958a4c430c51d322
Thingvellir <315338868@qq.com>
2017-11-09 21:45:16 +0800

    有意思初版

--- app/src/main/java/android/example/com/yihubaiying/fragment/Fragment_WoDe.java
+++ app/src/main/java/android/example/com/yihubaiying/fragment/Fragment_WoDe.java
@@ -20,7 +20,7 @@ public class Fragment_WoDe extends Fragment implements View.OnClickListener{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.adv_layout,container,false);
+        View view=inflater.inflate(R.layout.newsdetail_layout,container,false);
         initView(view);
         return view;
     }
